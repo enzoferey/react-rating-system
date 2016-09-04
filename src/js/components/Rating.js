452c1ff7f906c3c9aea1f242a07bd6858be3d1f8
Enzo Ferey <shoon96@gmail.com>
2016-09-05 01:48:29 +0200

    All image support, value as prop, color bg as prop, callback as prop, editable as prop.

--- src/js/components/Rating.js
+++ src/js/components/Rating.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 class Rating extends Component {
     constructor(props) { 
         super(props);
-        this.state = { currentHover: -1, rating: 0, preciseValue: 2.3 };
+        this.state = { currentHover: -1, rating: 0, preciseValue: this.props.initialValue };
     }
     handleOver(index) {
         this.setState({ currentHover: index });
@@ -17,62 +17,108 @@ class Rating extends Component {
     handleNumber(e) {
         this.setState({ preciseValue: e.target.value});
     }
-  render(){
-        let stars = new Array(5);
+    render(){
+        let stars = new Array(5);   
+        
+        if(this.props.editable) {
+            for (let i = 0; i < 5; i++) {
+                // Calculate width of the filling based on the value given
+                const style = {
+                    width: ((i + 1) <= this.state.preciseValue)? 98 + '%' : 
+                        (i < this.state.preciseValue)?
+                            (((this.state.preciseValue) % 1) * 100 - 2) + '%' : 0,
 
-    for (let i = 0; i < 5; i++) {
-            stars[i] = (
-                <div key={i} className="divStar">
-                    <img 
-                        onMouseEnter={this.handleOver.bind(this, i)}
-                        onClick={this.handleClick.bind(this, i)}
-                        src="../../square.png" alt=""
-                        style={{ background: (i<=this.state.currentHover)? 'pink' : '' }}
+                    background: this.props.bg
+                }
+
+                stars[i] = (
+                    <EditableStars 
+                        key={i} 
+                        image={this.props.image} 
+                        handleOver={this.handleOver.bind(this, i)} 
+                        callback={this.props.callback.bind(this, i)} 
+                        style={style} 
+                        iconStyle={{ background: (i <= this.state.currentHover)? this.props.bg : '' }} 
                     />
-                </div>
-            );
-    }
+                );
+            }
+        } else {
+            for (let i = 0; i < 5; i++) {
+                // Calculate width of the filling based on the value given
+                const style = {
+                    width: ((i + 1) <= this.state.preciseValue)? 98 + '%' : 
+                        (i < this.state.preciseValue)?
+                            (((this.state.preciseValue) % 1) * 100 - 2) + '%' : 0,
 
-    let stars2 = new Array(5);
+                    background: this.props.bg
+                }
 
-    for (let i = 0; i < 5; i++) {
-        // Calculate width of the filling based on the value given
-        const style = {
-            width: ((i + 1) <= this.state.preciseValue)? 98 + '%' : 
-                (i < this.state.preciseValue)?
-                    (((this.state.preciseValue) % 1) * 100 - 2) + '%' : 0
+                stars[i] = (
+                    <NonEditableStars key={i} image={this.props.image} style={style} />
+                );
+            }
         }
-
-        stars2[i] = (
-                <div key={i} className="divPreciseStar">
-                    <div className="fillSquare" style={style}></div>
-                    <img 
-                        src="../../square.png" alt=""
-                    />
-                </div>
-        );
-    }
-
-    return(
-        <div>
+        
+        return(
+            <div>
                 <div className="stars-container" onMouseLeave={this.handleLeave.bind(this)}>
                     {stars}
                 </div>
 
                 <p>
                     {(() => {
-                            if(this.state.rating > 0) 
-                                return`You rated the product with ${this.state.rating} star/s`;
+                        if(this.state.rating > 0) 
+                            return`You rated the product with ${this.state.rating} star/s`;
                     })()}
                 </p>
 
                 <input type="number" onChange={this.handleNumber.bind(this)} />
-                <div className="stars-container" onMouseLeave={this.handleLeave.bind(this)}>
-                    {stars2}
-                </div>
             </div>
+        );
+    }
+}
+
+const EditableStars = (props) => {
+    return (
+        <div className="divStar">
+            <div className="fillSquare" style={props.style}></div>
+            <img 
+                src={props.image} alt=""
+                onMouseEnter={props.handleOver}
+                onClick={props.callback}
+                style={props.iconStyle}
+            />
+        </div>
     );
-  }
+}
+
+const NonEditableStars = (props) => {
+    return (
+        <div className="divPreciseStar">
+            <div className="fillSquare" style={props.style}></div>
+            <img 
+                src={props.image} alt=""
+            />
+        </div>
+    );
+}
+
+
+
+Rating.propTypes = {
+    image: PropTypes.string.isRequired,
+    bg: PropTypes.string.isRequired,
+    initialValue: PropTypes.number,
+    editable: PropTypes.bool,
+    callback: PropTypes.func 
+}
+
+Rating.defaultProps = {
+    image: '../../square.png',
+    bg: 'pink',
+    initialValue: 0,
+    editable: true,
+    callback: Rating.prototype.handleClick
 }
 
 export default Rating;
