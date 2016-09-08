70aef43ddf6b9509cfe67a5a00b4894345d4e1d8
Enzo Ferey <shoon96@gmail.com>
2016-09-08 18:50:52 +0200

    Added example source link and renamed bundle name. Also added .npmignore

--- src/js/main.js
+++ src/js/main.js
@@ -9,9 +9,9 @@ const Examples = (props) => {
 	return (
 		<div style={{ fontFamily: 'Arial' }}>
   			<h1>Welcome to react-rating-system examples, please fasten your belt.</h1>
-        <h3>If you want to know how these examples were done, <a href="">click here to get the source code</a> !</h3>
+        	<h3>If you want to know how these examples were done, <a href="https://github.com/enzoferey/react-rating-system/blob/master/src/js/main.js">click here to get the source code</a> !</h3>
 
-        <br/>
+        	<br/>
   			<p>"This is the most basic example"</p>
   			<Rating image="star.png" fillBG="#f1c40f" containerStyle={{ maxWidth: '200px' }}/>
   			<br/><br/>
@@ -42,26 +42,26 @@ const Examples = (props) => {
   			
   			<p>"What if I want a function to be called when a star is clicked ?"</p>
   			<Rating image="star.png" fillBG="green" initialBG="pink" initialValue={3.5} callback={(index) => alert(`You rated my component with a ${index}`)}
-        containerStyle={{ maxWidth: '200px' }} />
-        <span style={{ marginLeft: '2em', position: 'relative', top: '0.75em', fontStyle: 'italic'}}>(click me !)</span>
+	        containerStyle={{ maxWidth: '200px' }} />
+	        <span style={{ marginLeft: '2em', position: 'relative', top: '0.75em', fontStyle: 'italic'}}>(click me !)</span>
   			<br/><br/>
 
   			<p>"This is nice, but if I want a 10 stars rating"</p>
   			<Rating image="star.png" fillBG="#f1c40f" numberStars={10} containerStyle={{ maxWidth: '200px' }}/>
-        <br/><br/>
+        	<br/><br/>
 
   			<p>"Okay, you did great until now, but now I want people to rate and then lock the stars value on their rating"</p>
   			<Rating image="star.png" fillBG="#f1c40f" numberStars={10} lockRating={true} containerStyle={{ maxWidth: '200px' }}/>
-        <span style={{ marginLeft: '2em', fontStyle: 'italic' }}>(click me and move away the mouse)</span>
+        	<span style={{ marginLeft: '2em', fontStyle: 'italic' }}>(click me and move away the mouse)</span>
   			<br/><br/>
 
   			<p>"Yo, those stars look horrible, can I put any other image I want ?"</p>
   			<Rating image="heart.png" fillBG="red" numberStars={5} containerStyle={{ maxWidth: '200px' }}/>
-        <br/><br/>
+        	<br/><br/>
 
-        <p>"Another example ?"</p>
-        <Rating image="star2.png" fillBG="#f1c40f" initialBG="#6a6a6a" numberStars={5} containerStyle={{ maxWidth: '200px' }}/>
-        <br/><br/><br/>
+	        <p>"Another example ?"</p>
+	        <Rating image="star2.png" fillBG="#f1c40f" initialBG="#6a6a6a" numberStars={5} containerStyle={{ maxWidth: '200px' }}/>
+	        <br/><br/><br/>
 		</div>
 	);
 }
